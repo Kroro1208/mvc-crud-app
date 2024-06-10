48fc3932535dbe6fb7f19f86af0d525fe9c539c0
kuroro1208 <yamagata_7580@yahoo.co.jp>
2024-06-10 19:37:39 +0900

    読み込みファイルを切り分けた

--- index.php
+++ index.php
@@ -1,3 +1,22 @@
 <?php
 
 require_once "./autoload.php";
+if (isset($_GET['views'])) {
+    $url = explode('/', $_GET['views']);
+} else {
+    $url = ["login"];
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <?php require_once "./app/views/inc/head.php"; ?>
+</head>
+
+<body>
+    <?php require_once "./app/views/inc/script.php"; ?>
+</body>
+
+</html>
